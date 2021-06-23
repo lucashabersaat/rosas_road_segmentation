b76f384af448e080757b37eeb447485e3a335d78
Lucas Habersaat <lucas@habersaat.ch>
2021-06-23 10:22:22 +0200

    Choose model for the lighting setup by system argument

--- methods/lightning/main.py
+++ methods/lightning/main.py
@@ -1,12 +1,12 @@
-import torch
+import sys
 
+import torch
 import pytorch_lightning as pl
 
 from models.unet import UNet
 from models.unet_self_attention import UNetSelfAttention
 
 from common.lightning.base import LitBase
-
 from common.lightning.road_data_module import RoadDataModule
 from common.predict import predict_and_write_submission
 
@@ -30,6 +30,17 @@ def load_model_and_write_submission(path, model_class):
 
 
 if __name__ == '__main__':
+    """The specific model can be given as argument to the program."""
+    if sys.argv is not None:
+        model_name = sys.argv[1]
+        if model_name == 'unet':
+            arg_model = UNet()
+        elif model_name == 'self_attention_unet':
+            arg_model = UNetSelfAttention()
+        else:
+            raise Exception('unknown model')
+    else:
+        arg_model = UNet()
+
+    fit_normally(LitBase(arg_model))
     # load_model_and_write_submission(LitUNet, "lightning_logs/version_4/checkpoints/epoch=53-step=1241.ckpt")
-    fit_normally(LitBase(UNet()))
-    fit_normally(LitBase(UNetSelfAttention()))
