a6b8b009e34bc66f9ae20c2df748d42df773f0d1
Lucas Habersaat <lucas@habersaat.ch>
2021-06-25 15:35:36 +0200

    Add unet transformers (#5)
    * Add unet transformers
    
    * Shift resize_to to own variable and reduce batch_size
    
    * Add utility print functions for tensors and models and memory
    
    * Make UNetTransformers work by reducing memory footprint by using a smaller image size
    
    Co-authored-by: Robin Bisping <robin@bisping.ch>

--- common/util.py
+++ common/util.py
@@ -8,12 +8,43 @@ def np_to_tensor(x, device):
     else:
         return (
             torch.from_numpy(x)
-            .contiguous()
-            .pin_memory()
-            .to(device=device, non_blocking=True)
+                .contiguous()
+                .pin_memory()
+                .to(device=device, non_blocking=True)
         )
 
 
 def accuracy_fn(y_hat, y):
     # computes classification accuracy
     return (y_hat.round() == y.round()).float().mean()
+
+
+def print_model_memory(model):
+    """Print how much memory the model utilized."""
+    mem = 0
+    for name, param in model.state_dict().items():
+        if len(param.shape) != 0:
+            tmp_mem = param.element_size() * param.nelement()
+            mem += tmp_mem
+    print_memory(mem)
+
+
+def print_tensor_info(t, name=None):
+    """Print shape & num elements of a tensor."""
+    if name is not None:
+        print(name, end=': ')
+
+    print("Size:", list(t.shape), "(" + str(t.nelement()) + ")", end=' ')
+
+    size = t.element_size() * t.nelement()
+    print_memory(size)
+
+
+def print_memory(size):
+    """Given a byte number, print out a suitable conversion to kb, mb or gb."""
+    if size > 1024 ** 3:
+        print(size / 1024 / 1024 / 1024, "gb")
+    elif size > 1024 ** 2:
+        print(size / 1024 / 1024, "mb")
+    elif size > 1024:
+        print(size / 1024 / 1024, "kb")
--- methods/unet.py
+++ methods/unet.py
@@ -2,21 +2,24 @@ import torch
 from torch import nn
 
 from common.read_data import *
-from common.util import np_to_tensor, accuracy_fn
+from common.util import np_to_tensor, accuracy_fn, print_model_memory
 from common.image_data_set import ImageDataSet
 from methods.conv_neural_networks import train
 from models.unet import UNet, patch_accuracy_fn
 
 if __name__ == "__main__":
 
+    torch.cuda.empty_cache()
+    resize_to = 384
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     batch_size = 4
 
     train_dataset = ImageDataSet(
-        "data/training", device, use_patches=False, resize_to=(384, 384)
+        "data/training", device, use_patches=False, resize_to=(resize_to, resize_to)
     )
     val_dataset = ImageDataSet(
-        "data/validation", device, use_patches=False, resize_to=(384, 384)
+        "data/validation", device, use_patches=False, resize_to=(resize_to, resize_to)
     )
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset, batch_size=batch_size, shuffle=True
@@ -52,7 +55,7 @@ if __name__ == "__main__":
 
     # we also need to resize the test images. This might not be the best idea depending on their spatial resolution
     test_images = np.stack(
-        [cv2.resize(img, dsize=(384, 384)) for img in test_images], 0
+        [cv2.resize(img, dsize=(resize_to, resize_to)) for img in test_images], 0
     )
     test_images = np_to_tensor(np.moveaxis(test_images, -1, 1), device)
 
