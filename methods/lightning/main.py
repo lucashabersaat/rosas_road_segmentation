4155e04dbd585b07f3388f1c23aa3898b2b9baad
Lucas Habersaat <lucas@habersaat.ch>
2021-06-29 15:37:23 +0200

    Add unet transformer to lightning

--- methods/lightning/main.py
+++ methods/lightning/main.py
@@ -5,22 +5,22 @@ import pytorch_lightning as pl
 
 from models.unet import UNet
 from models.unet_self_attention import UNetSelfAttention
+from models.unet_transformer import U_Transformer
 
 from common.lightning.base import LitBase
 from common.lightning.road_data_module import RoadDataModule
 from common.predict import predict_and_write_submission
 
 
-def fit_normally(model):
-    data = RoadDataModule()
+def fit_normally(model, data):
 
-    gpu = torch.cuda.is_available()
-    trainer = pl.Trainer(gpus=gpu, max_epochs=50)
+    gpu = int(torch.cuda.is_available())
+    trainer = pl.Trainer(gpus=gpu, max_epochs=50, logger=None)
 
     trainer.fit(model, data)
 
-    name = 'lightning_' + str.lower(arg_model.__class__.__name__)
-    predict_and_write_submission(model, name)
+    name = 'lightning_' + str.lower(model.model.__class__.__name__)
+    predict_and_write_submission(model, name, resize_to=192)
 
 
 def load_model_and_write_submission(path, model_class):
@@ -37,13 +37,19 @@ if __name__ == '__main__':
     if sys.argv is not None:
         model_name = sys.argv[1]
         if model_name == 'unet':
-            arg_model = UNet()
+            model = LitBase(UNet())
+            data = RoadDataModule(resize_to=384)
+        elif model_name == 'unet_transformer':
+            model = LitBase(U_Transformer(3, 1), loss_fn='noise_robust_dice')
+            data = RoadDataModule(batch_size=1,  resize_to=192)
         elif model_name == 'self_attention_unet':
-            arg_model = UNetSelfAttention()
+            model = LitBase(UNetSelfAttention())
+            data = RoadDataModule(resize_to=384)
         else:
             raise Exception('unknown model')
     else:
-        arg_model = UNet()
+        model = LitBase(UNet())
+        data = RoadDataModule(resize_to=384)
 
-    fit_normally(LitBase(arg_model))
-    # load_model_and_write_submission(LitUNet, "lightning_logs/version_4/checkpoints/epoch=53-step=1241.ckpt")
+    fit_normally(model, data)
+    # load_model_and_write_submission(LitUNet, "lightning_logs_old/version_4/checkpoints/epoch=53-step=1241.ckpt")
