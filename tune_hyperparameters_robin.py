fa68b186ba428ea8c8b5863878e4b18fc7a7d349
sam <sbedassa67@gmail.com>
2021-07-28 11:09:09 +0200

    changed local dir

--- tune_hyperparameters_robin.py
+++ tune_hyperparameters_robin.py
@@ -116,6 +116,7 @@ if __name__ == "__main__":
         mode="max",
         config=config,
         num_samples=num_samples,
+        local_dir=/cluster/scratch/samuelbe,
         name="tune_segmentation_config_robin_1")
 
     print("stayin alive, aha aha aha")
